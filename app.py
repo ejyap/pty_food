023de39cc1e6b8c2cee61beb1cd5065bd6e39a18
Eduardo Yap <eduardojyap@gmail.com>
2019-06-27 13:40:25 -0500

    download csv from web

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 from dash.dependencies import Input, Output
 from flask import Flask
 
-df = pd.read_csv('data\\processed\\city_restaurants_filtered.csv', encoding='utf-8')
+df = pd.read_csv('https://raw.githubusercontent.com/ejyap/pty_food/master/data/processed/city_restaurants_filtered.csv')
 
 colors = ['#1f77b4','#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b',  '#e377c2', '#7f7f7f',  '#bcbd22', '#17becf', 'cyan']
 
